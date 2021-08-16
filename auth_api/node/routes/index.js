8fa0962d0f5c7bf9dfb41f80337cdc54f057461d
marcxz <marcxz@gmail.com>
2021-08-15 22:04:52 -0500

    fix: fix aesthetic code and add the final packages

--- auth_api/node/routes/index.js
+++ auth_api/node/routes/index.js
@@ -2,9 +2,8 @@ import { login } from './login';
 import { protect } from './protected';
 import { health } from '../services/health';
 import { JwtService } from '../services/jwtService';
-import { DataBase } from '../services/db';
 const jwtService = new JwtService();
-const db = new DataBase();
+
 export const init = async (app) => {
   app.post('/login', login);
   app.get('/protected', jwtService.isAuth, protect);
